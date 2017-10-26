63d431bc9fc0f477dea8aaea004b77390aff776d
ne-peer <mileage0110@gmail.com>
2017-10-26 20:51:33 +0900

    [fix] title

--- script/02_generate-result-summary.js
+++ script/02_generate-result-summary.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 
 const generateDate = '2017.10.26';
+const title = 'デレステキャラソート サマリ';
+
 const template = `
 <!DOCTYPE html>
 <html lang="ja">
@@ -9,7 +11,7 @@ const template = `
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>${title} ${generateDate}</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.css.map">
 </head>
