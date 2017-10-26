83ee6390bd308bda587160e2d7cf9e9acf9832be
ne-peer <mileage0110@gmail.com>
2017-10-27 01:00:34 +0900

    [fix] 要素内の幅を最大に

--- script/02_generate-result-summary.js
+++ script/02_generate-result-summary.js
@@ -17,6 +17,11 @@ const template = `
 </head>
 
 <body>
+<style>
+    table.table {
+        width: 100%;
+    }
+</style>
     <section class="hero is-medium is-primary is-bold">
         <div class="hero-body">
             <div class="container">
